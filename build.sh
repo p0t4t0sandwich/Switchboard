7587352bd9c436c186e698569b54d5df5e153d7d
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2023-10-26 02:40:49 -0600

    Updated build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 PROJ_ID=tatercomms
 PROJ_NAME=TaterComms
-VERSION=1.0.3-R01-SNAPSHOT
+VERSION=1.0.3
 GROUP_ID=dev/neuralnexus
 
 # --------------------------- Functions --------------------------------
@@ -38,6 +38,7 @@ function build() {
   cp ./forge-$2/pack.mcmeta ./$3
   cp -r ./forge-$2/$PROJ_NAME.png ./$3
   cp ./forge-$2/META-INF/mods.toml ./$3/META-INF
+  cp ./forge-$2/mcmod.info ./$3
 
   # Zip Jar contents
   cd ./$3
@@ -47,97 +48,120 @@ function build() {
   # Rename Jar
   mv ./$3.zip ./$3.jar
 
-  # Generate MD5
-  md5sum ./$3.jar | cut -d ' ' -f 1 > ./$3.jar.MD5
+  # Generate hashes
+  md5sum ./$3.jar | cut -d ' ' -f 1 > ./$3.jar.md5
+  mv ./$3.jar.md5 ../$3.jar.md5
+  sha1sum ./$3.jar | cut -d ' ' -f 1 > ./$3.jar.sha1
+  mv ./$3.jar.sha1 ../$3.jar.sha1
+  sha256sum ./$3.jar | cut -d ' ' -f 1 > ./$3.jar.sha256
+  mv ./$3.jar.sha256 ../$3.jar.sha256
+  sha512sum ./$3.jar | cut -d ' ' -f 1 > ./$3.jar.sha512
+  mv ./$3.jar.sha512 ../$3.jar.sha512
 
   # Move Jar
   mv ./$3.jar ../$3.jar
-  mv ./$3.jar.MD5 ../$3.jar.MD5
 }
 
 function spongebuild() {
-    echo "Building using Forge $2, Fabric $1 and Sponge $3"
+  echo "Building using Forge $2, Fabric $1 and Sponge $3"
 
-      mkdir -p ./$4
-      mkdir -p ./$4/META-INF
+    mkdir -p ./$4
+    mkdir -p ./$4/META-INF
 
-      # Copy common files
-      cp -r ./$PROJ_NAME-all/* ./$4/
+    # Copy common files
+    cp -r ./$PROJ_NAME-all/* ./$4/
 
-      # Copy fabric files
-      cp -r ./fabric-$1/$GROUP_ID/$PROJ_ID/fabric ./$4/$GROUP_ID/$PROJ_ID
-      cp ./fabric-$1/fabric.mod.json ./$4
-      cp ./fabric-$1/$PROJ_ID.mixins.json ./$4
-      cp -r ./fabric-$1/assets ./$4
-      cp ./fabric-$1/fabric-$1-refmap.json ./$4
-      cp -r ./fabric-$1/META-INF/jars ./$4/META-INF
+    # Copy fabric files
+    cp -r ./fabric-$1/$GROUP_ID/$PROJ_ID/fabric ./$4/$GROUP_ID/$PROJ_ID
+    cp ./fabric-$1/fabric.mod.json ./$4
+    cp ./fabric-$1/$PROJ_ID.mixins.json ./$4
+    cp -r ./fabric-$1/assets ./$4
+    cp ./fabric-$1/fabric-$1-refmap.json ./$4
+    cp -r ./fabric-$1/META-INF/jars ./$4/META-INF
 
-      # Copy forge files
-      cp -r ./forge-$2/$GROUP_ID/$PROJ_ID/forge ./$4/$GROUP_ID/$PROJ_ID
-      cp ./forge-$2/pack.mcmeta ./$4
-      cp -r ./forge-$2/$PROJ_NAME.png ./$4
-      cp ./forge-$2/META-INF/mods.toml ./$4/META-INF
+    # Copy forge files
+    cp -r ./forge-$2/$GROUP_ID/$PROJ_ID/forge ./$4/$GROUP_ID/$PROJ_ID
+    cp ./forge-$2/pack.mcmeta ./$4
+    cp -r ./forge-$2/$PROJ_NAME.png ./$4
+    cp ./forge-$2/META-INF/mods.toml ./$4/META-INF
+    cp ./forge-$2/mcmod.info ./$4
 
-      # Copy sponge files
-      cp -r ./sponge$3/$GROUP_ID/$PROJ_ID/sponge ./$4/$GROUP_ID/$PROJ_ID
-      cp ./sponge$3/META-INF/sponge_plugins.json ./$4/META-INF
+    # Copy sponge files
+    cp -r ./sponge$3/$GROUP_ID/$PROJ_ID/sponge ./$4/$GROUP_ID/$PROJ_ID
+    cp ./sponge$3/META-INF/sponge_plugins.json ./$4/META-INF
 
-      # Zip Jar contents
-      cd ./$4
-      zip -qr ../$4.zip ./*
-      cd ../
+    # Zip Jar contents
+    cd ./$4
+    zip -qr ../$4.zip ./*
+    cd ../
 
-      # Rename Jar
-      mv ./$4.zip ./$4.jar
+    # Rename Jar
+    mv ./$4.zip ./$4.jar
 
-      # Generate MD5
-      md5sum ./$4.jar | cut -d ' ' -f 1 > ./$4.jar.MD5
+    # Generate hashes
+    md5sum ./$4.jar | cut -d ' ' -f 1 > ./$4.jar.md5
+    mv ./$4.jar.md5 ../$4.jar.md5
+    sha1sum ./$4.jar | cut -d ' ' -f 1 > ./$4.jar.sha1
+    mv ./$4.jar.sha1 ../$4.jar.sha1
+    sha256sum ./$4.jar | cut -d ' ' -f 1 > ./$4.jar.sha256
+    mv ./$4.jar.sha256 ../$4.jar.sha256
+    sha512sum ./$4.jar | cut -d ' ' -f 1 > ./$4.jar.sha512
+    mv ./$4.jar.sha512 ../$4.jar.sha512
 
-      # Move Jar
-      mv ./$4.jar ../$4.jar
-      mv ./$4.jar.MD5 ../$4.jar.MD5
+    # Move Jar
+    mv ./$4.jar ../$4.jar
 }
 
 function neobuild() {
-  echo "Building using Forge $2, Fabric $1 and NeoForge $3"
+  echo "Building using Forge $2, Fabric $1, Sponge $3, and NeoForge $4"
 
-  mkdir -p ./$4
-  mkdir -p ./$4/META-INF
+  mkdir -p ./$5
+  mkdir -p ./$5/META-INF
 
   # Copy common files
-  cp -r ./$PROJ_NAME-all/* ./$4/
+  cp -r ./$PROJ_NAME-all/* ./$5/
 
   # Copy fabric files
-  cp -r ./fabric-$1/$GROUP_ID/$PROJ_ID/fabric ./$4/$GROUP_ID/$PROJ_ID
-  cp ./fabric-$1/fabric.mod.json ./$4
-  cp ./fabric-$1/$PROJ_ID.mixins.json ./$4
-  cp -r ./fabric-$1/assets ./$4
-  cp ./fabric-$1/fabric-$1-refmap.json ./$4
-  cp -r ./fabric-$1/META-INF/jars ./$4/META-INF
+  cp -r ./fabric-$1/$GROUP_ID/$PROJ_ID/fabric ./$5/$GROUP_ID/$PROJ_ID
+  cp ./fabric-$1/fabric.mod.json ./$5
+  cp ./fabric-$1/$PROJ_ID.mixins.json ./$5
+  cp -r ./fabric-$1/assets ./$5
+  cp ./fabric-$1/fabric-$1-refmap.json ./$5
+  cp -r ./fabric-$1/META-INF/jars ./$5/META-INF
 
   # Copy forge files
-  cp -r ./forge-$2/$GROUP_ID/$PROJ_ID/forge ./$4/$GROUP_ID/$PROJ_ID
-  cp ./forge-$2/pack.mcmeta ./$4
-  cp -r ./forge-$2/$PROJ_NAME.png ./$4
-  cp ./forge-$2/META-INF/mods.toml ./$4/META-INF
+  cp -r ./forge-$2/$GROUP_ID/$PROJ_ID/forge ./$5/$GROUP_ID/$PROJ_ID
+  cp ./forge-$2/pack.mcmeta ./$5
+  cp -r ./forge-$2/$PROJ_NAME.png ./$5
+  cp ./forge-$2/META-INF/mods.toml ./$5/META-INF
+
+  # Copy sponge files
+  cp -r ./sponge$3/$GROUP_ID/$PROJ_ID/sponge ./$5/$GROUP_ID/$PROJ_ID
+  cp ./sponge$3/META-INF/sponge_plugins.json ./$5/META-INF
 
   # Copy neoforge files
-  cp -r ./neoforge-$3/$GROUP_ID/$PROJ_ID/neoforge ./$4/$GROUP_ID/$PROJ_ID
+  cp -r ./neoforge-$4/$GROUP_ID/$PROJ_ID/neoforge ./$5/$GROUP_ID/$PROJ_ID
 
   # Zip Jar contents
-  cd ./$4
-  zip -qr ../$4.zip ./*
+  cd ./$5
+  zip -qr ../$5.zip ./*
   cd ../
 
   # Rename Jar
-  mv ./$4.zip ./$4.jar
+  mv ./$5.zip ./$5.jar
 
-  # Generate MD5
-  md5sum ./$4.jar | cut -d ' ' -f 1 > ./$4.jar.MD5
+  # Generate hashes
+  md5sum ./$5.jar | cut -d ' ' -f 1 > ./$5.jar.md5
+  mv ./$5.jar.md5 ../$5.jar.md5
+  sha1sum ./$5.jar | cut -d ' ' -f 1 > ./$5.jar.sha1
+  mv ./$5.jar.sha1 ../$5.jar.sha1
+  sha256sum ./$5.jar | cut -d ' ' -f 1 > ./$5.jar.sha256
+  mv ./$5.jar.sha256 ../$5.jar.sha256
+  sha512sum ./$5.jar | cut -d ' ' -f 1 > ./$5.jar.sha512
+  mv ./$5.jar.sha512 ../$5.jar.sha512
 
   # Move Jar
-  mv ./$4.jar ../$4.jar
-  mv ./$4.jar.MD5 ../$4.jar.MD5
+  mv ./$5.jar ../$5.jar
 }
 
 # --------------------------- Setup --------------------------------
