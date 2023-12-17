89d58e14059f283ccefe7c335bef00a8c45b1541
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2023-12-16 21:50:47 -0600

    Forge 1.12 and below support

--- platforms/forge/src/main/java/dev/neuralnexus/tatercomms/platforms/ForgePlugin.java
+++ platforms/forge/src/main/java/dev/neuralnexus/tatercomms/platforms/ForgePlugin.java
@@ -7,7 +7,12 @@ import dev.neuralnexus.taterlib.logger.LoggerAdapter;
 import net.minecraftforge.fml.common.Mod;
 
 /** Forge entry point. */
-@Mod(TaterComms.Constants.PROJECT_ID)
+@Mod(
+        value = TaterComms.Constants.PROJECT_ID,
+        modid = TaterComms.Constants.PROJECT_ID,
+        useMetadata = true,
+        serverSideOnly = true,
+        acceptableRemoteVersions = "*")
 public class ForgePlugin implements TaterCommsPlugin {
     public ForgePlugin() {
         pluginStart(this, new LoggerAdapter(TaterComms.Constants.PROJECT_NAME));
